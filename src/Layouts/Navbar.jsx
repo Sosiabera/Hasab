acb93ded0a0f005faeeda6ffc45312b77cf6c45e
yanitbee <yanithaftomm@gmail.com>
2024-11-04 12:25:03 +0100

    footer

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -5,11 +5,34 @@ const Navbar =() =>{
 
   return (
     <nav className="bg-orange-500 text-white">
-      <div className="container mx-auto flex justify-between items-center p-4">
+      <img src="/image/hlogo.JPG" alt="" class="absolute w-12 h-12 rounded-full group"/>
+      <div className="container mx-auto flex justify-between items-center p-2">
         <div className="text-2xl font-bold">
         </div>
-        <img src="/image/hlogo.png" alt="" />
-       
+<div
+  class="hidden md:flex p-5 overflow-hidden w-[60px] h-[20px] hover:w-[270px] bg-white shadow-[2px_2px_20px_rgba(0,0,0,0.08)] rounded-full flex group items-center hover:duration-300 duration-300"
+>
+  <div class="flex items-center justify-center fill-black">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="Isolation_Mode"
+      data-name="Isolation Mode"
+      viewBox="0 0 24 24"
+      width="15"
+      height="15"
+    >
+      <path
+        d="M18.9,16.776A10.539,10.539,0,1,0,16.776,18.9l5.1,5.1L24,21.88ZM10.5,18A7.5,7.5,0,1,1,18,10.5,7.507,7.507,0,0,1,10.5,18Z"
+      ></path>
+    </svg>
+  </div>
+  <input
+    type="text"
+    class="outline-none text-[15px] bg-transparent w-full text-black font-sm px-4"
+  />
+</div>
+
+
 
         <div className="md:hidden">
           <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
@@ -29,16 +52,15 @@ const Navbar =() =>{
               />
             </svg>
           </button>
+          
         </div>
       </div>
-
-      <div className="hidden md:flex space-x-6">
-          <a href="/" className="hover:text-gray-400">News</a>
-          <a href="/Blog" className="hover:text-gray-400">Blog</a>
-          <a href="/Donations" className="hover:text-gray-400">Donations</a>
-          <a href="/Support" className="hover:text-gray-400">Support</a>
+      <div className="hidden md:flex space-x-6 bg-white text-black text-sm">
+          <a href="/" className="hover:text-orange-400">News</a>
+          <a href="/Blog" className="hover:text-orange-400">Blog</a>
+          <a href="/Donations" className="hover:text-orange-400">Donations</a>
+          <a href="/Support" className="hover:text-orange-400">Support</a>
         </div>
-
       {isOpen && (
         <div className="md:hidden bg-orange-500">
           <a href="/" className="block py-2 px-4 hover:bg-gray-400">News</a>
