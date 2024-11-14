d37ec350f3221cf1584edd980f129221d6b806ff
sosiabera <sosiabera32@gmail.com>
2024-11-14 07:50:52 -0400

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css';
 import Navbar from './Layouts/Navbar';
 import Footer from './Layouts/Footer';
+import Landing from './Pages/Landing';
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <Landing />
 
-      <div className="flex-grow">
+      <div className="flex-grow ">
+       
        
       </div>
 
---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,29 @@
+
+
+
+
+ export default function Landing() {
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="">
+<p>News</p>
+<p>2017</p>
+<div>
+<p>LIVE: Dozens of bodies on northern Gaza streets during Israeli siege</p>
+<button>Read</button>
+</div>
+
+<div>
+    <img src="/image/gaza.jpg"></img>
+</div>
+
+            </div>
+
+            <div>
+
+            </div>
+            
+        </div>
+    )
+}
